7a2d9d543b7acaee4f99a72f463e31bc292425f4
Antonio Lieto <antonio.lieto1999@gmail.com>
2025-08-01 09:28:01 +0200

    initial commit del progetto

---
+++ src/main/java/Antonio/U5_W3_D5_progetto/U5W3D5ProgettoApplication.java
@@ -0,0 +1,13 @@
+package Antonio.U5_W3_D5_progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5ProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Antonio/U5_W3_D5_progetto/U5W3D5ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package Antonio.U5_W3_D5_progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
